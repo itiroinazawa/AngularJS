12d9c4b88433e1da7944124c3546df077f4ddb90
Itiro <itiro.inazawa@hotmail.com>
2015-12-13 20:44:48 -0200

    [Rodrigo Itiro] - Roteamento de views com passagem de parametros por Resolve

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,23 @@
 
 angular.module('myApp', ['myApp.controllers', 'ngRoute']);
 
-angular.module('myApp').config(function ($routeProvider) {
+angular.module('myApp').config(function ($routeProvider, $locationProvider) {
     $routeProvider
         .when('/view1', {
             controller: 'Controller1',
             templateUrl: 'views/partials/view1.html'
         })
-        .when('/view2', {
+        .when('/view2/:firstname/:lastname', {
             controller: 'Controller2',
-            templateUrl: 'views/partials/view2.html'
+            templateUrl: 'views/partials/view2.html',
+            resolve: {
+                names: function () {
+                    //typically you will use a service to retrieve values from the server here
+                    return ['Misko', 'Vojta', 'Brad']; //this is used as dependency value
+                }
+            }
+        }).otherwise({
+            redirectTo: '/view1'
         });
+    $locationProvider.html5Mode(true);
 });
-
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,11 @@
-angular.module('myApp.controllers', []).controller('Controller1',function($scope){
-    $scope.message="Hello, world";
-}).controller('Controller2',function($scope){
-    $scope.now=new Date();
+'use strict';
+
+angular.module('myApp.controllers', []).controller('Controller1', function($scope,$location){
+    $scope.loadView2=function(){
+        $location.path('/view2/' + $scope.firstname+'/' + $scope.lastname);
+    }
+}).controller('Controller2',function($scope,$routeParams,names){ //names is now a dependency
+    $scope.firstname=$routeParams.firstname;
+    $scope.lastname=$routeParams.lastname;
+    $scope.names=names;
 });
