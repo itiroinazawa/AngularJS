9a1494479f869a6be3dc18ef4278dc1758790ff3
Itiro <itiro.inazawa@hotmail.com>
2015-12-17 23:14:48 -0200

    Inclusao de diretiva para comentarios

--- app/modules/posts/js/directives.js
+++ app/modules/posts/js/directives.js
@@ -1,3 +1,24 @@
 'use strict'
 
-angular.module('spBlogger.posts.directives',[]);
+angular.module('spBlogger.posts.directives',[]).directive('spbComments',function(Post){
+    return {
+        restrict:'AEC',
+        scope:{
+            postInstance:'='
+        },
+        replace:true,
+        link:function(scope,elem,attrs){
+            scope.saveComment=function(){
+                var postID=scope.postInstance._id,
+                    savedPostInstance={};
+                scope.comment.datePublished=new Date(); 
+                angular.copy(scope.postInstance,savedPostInstance); 
+                savedPostInstance.comments.unshift(scope.comment); 
+                scope.postInstance.comments.unshift(scope.comment);
+                scope.comment={}; // clear the comment
+                savedPostInstance.$update(); //Now update `savedPostInstance`
+            }
+        },
+        templateUrl:'app/modules/posts/views/comments.html'
+    }
+});
