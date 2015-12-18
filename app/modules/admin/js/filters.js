3a359fc4d78394bfb5a7a071c1a99d9cef1b6d60
Itiro <itiro.inazawa@hotmail.com>
2015-12-17 23:26:51 -0200

    Inclusao de filtros customizados

--- app/modules/admin/js/filters.js
+++ app/modules/admin/js/filters.js
@@ -1,3 +1,13 @@
-'use strict'
+'use strict';
 
-angular.module('spBlogger.admin.filters',[]);
+angular.module('spBlogger.admin.filters',[]).filter('permalink',function(){
+    return function(title){
+        return title===undefined ? '' : angular.lowercase(title).replace(/[\s]/g,'-');
+    }
+});
+
+angular.module('spBlogger.admin.filters').filter('wordcount',function(){
+    return function(input){
+        return input===undefined ? 0 : input.split(/\s/g).length;
+    }
+});
